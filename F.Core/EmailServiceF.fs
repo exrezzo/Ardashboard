cb41d64fd5b1c537b1c300d2c1b6836119d9ac82
Ivan Ardillo <ivan.ardillo@configuratori.com>
2022-08-31 17:17:54 +0200

    html to bank transaction entity

--- F.Core/EmailServiceF.fs
+++ F.Core/EmailServiceF.fs
@@ -1,11 +1,64 @@
 ﻿module F.Core.EmailService
 
+open System
 open System.Data.SQLite
+open System.IO
+open System.Text.RegularExpressions
+open System.Web
 open Ardashboard.Infrastructure
+open FSharp.Data
 open RepoDb
 
-
 type HtmlBankMsg = { Id: string; HtmlBody: string }
+
+module BankTransaction =
+    type BankTransaction =
+        { Amount: double
+          Place: string
+          Occurred: DateTime }
+
+    let private getTextFromHtmlNode (html: HtmlNode) =
+        let htmlDecode =
+            HttpUtility.HtmlDecode(html.InnerText())
+
+        Regex.Replace(htmlDecode, @"\s+", " ").Trim()
+
+    let fromHtmlBankMsg (htmlBankMsg: HtmlBankMsg) =
+        let table =
+            HtmlDocument
+                .Load(new StringReader(htmlBankMsg.HtmlBody))
+                .CssSelect "table.tabellaDisposizione"
+            |> Seq.head
+
+        let transactionKeyValues =
+            table.Descendants["tr"]
+            |> Seq.map (fun tableRow ->
+                (tableRow.Descendants["th"]
+                 |> Seq.head
+                 |> getTextFromHtmlNode,
+                 tableRow.Descendants["td"]
+                 |> Seq.head
+                 |> getTextFromHtmlNode))
+
+        let mutable amount = 0.0
+        let mutable place = ""
+        let mutable date = DateTime.Now
+        let mutable hour = TimeOnly.MinValue
+
+        transactionKeyValues
+        |> Seq.iter (fun (row) ->
+            match row with
+            | ("Importo", value) -> amount <- Double.Parse(value.Replace("euro", "").Trim())
+            | ("Presso", value) -> place <- value
+            | ("Data", value) -> date <- DateTime.Parse value
+            | ("Ora", value) -> hour <- TimeOnly.Parse value
+            | _ -> failwith "Unknown header in table for transaction")
+
+
+        { Amount = amount
+          Place = place
+          Occurred = date + hour.ToTimeSpan() }
+
 
 module GmailServiceModule =
     open System.Threading
@@ -158,3 +211,4 @@ module EmailServiceModule =
 
         htmlBankMessagesFromApi
         |> Seq.append (htmlBankMessagesFromStore)
+        |> Seq.map BankTransaction.fromHtmlBankMsg
